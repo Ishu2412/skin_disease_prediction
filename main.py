4b912a3e6ba88825aae11590b6702d1591eccf19
Shrayash Shukla <98635559+Ishu2412@users.noreply.github.com>
2023-09-19 00:48:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
-from PIL import Image
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras import Model
 import cv2
-import os
 import numpy as np
+from fastapi import FastAPI
+import skimage
 
-train2 = ['Seborrheic Keratoses and other Benign Tumors',
+app = FastAPI()
+
+
+label = ['Seborrheic Keratoses and other Benign Tumors',
  'Psoriasis pictures Lichen Planus and related diseases',
  'Tinea Ringworm Candidiasis and other Fungal Infections',
  'Eczema Photos',
@@ -14,15 +15,15 @@ train2 = ['Seborrheic Keratoses and other Benign Tumors',
 
 vgg_model = tf.keras.applications.vgg19.VGG19(weights = 'imagenet',  include_top = False, input_shape = (180, 180, 3))
 
+# Load saved model
+model = tf.keras.models.load_model('skin.h5')
+
 def predict_skin_disease(image_path):
    # Define list of class names
-    class_names = train2
+    class_names = label
 
-    # Load saved model
-    model = tf.keras.models.load_model('skin.h5')
-
-    # Load and preprocess image
-    img = cv2.imread(image_path)
+    print(image_path)
+    img = skimage.io.imread( image_path )
     img = cv2.resize(img, (180, 180))
     img = np.array(img)
     img = np.expand_dims(img, axis=0)
@@ -40,17 +41,10 @@ def predict_skin_disease(image_path):
 
 # print(predict_skin_disease(r"D:\Downloads\contest\SIH\Data Set Skin Disease\train\Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions\actinic-cheilitis-sq-cell-lip-1.jpg"))
 
-# Importing Necessary modules
-from fastapi import FastAPI
-import uvicorn
- 
-# Declaring our FastAPI instance
-app = FastAPI()
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def greet():
+    return "Hello World"
 
 @app.post("/predict")
-def image_detect(image_path) :
-    return {"prediction" : predict_skin_disease(image_path)}
+def predict(image_path) :
+    return predict_skin_disease(image_path)
